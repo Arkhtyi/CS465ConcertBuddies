6299de9358aa528f9f7d01da2d6a4ceacc9782e7
Salina Ortega <sortega2@illinois.edu>
2017-11-28 17:14:14 -0600

    adding button events

--- ConcertBuddies/app/src/main/java/com/example/arkh_tyi/concertbuddies/MainActivity.java
+++ ConcertBuddies/app/src/main/java/com/example/arkh_tyi/concertbuddies/MainActivity.java
@@ -3,15 +3,58 @@ package com.example.arkh_tyi.concertbuddies;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button MyBuddies;
+    private Button RecommendBuddies;
+    private Button MyEvents;
+    private Button RecommendEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_screen);
 
-        Intent intent = new Intent(this, UserProfileActivity.class);
-        startActivity(intent);
+        MyBuddies = (Button) findViewById(R.id.MyBuddiesButton);
+        RecommendBuddies = (Button) findViewById(R.id.RecommendedBuddiesButton);
+        MyEvents = (Button) findViewById(R.id.MyEventsButton);
+        RecommendEvents = (Button) findViewById(R.id.RecommendedEventsButton);
+
+
+
+        MyBuddies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ));
+            }
+        });
+
+        RecommendBuddies.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ));
+            }
+        });
+
+        MyEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ));
+            }
+        });
+
+        RecommendEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ));
+            }
+        });
+
+
     }
+
+
 }
